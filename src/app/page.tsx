9ea11d7f3769de8c2584e387e820bf15cc33ff85
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-11-17 15:29:40 +0800

    Test Redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import { useEffect } from 'react';
 // import { currentUser } from '@clerk/nextjs';
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.replace('/auth-callback');
-  }, [])
+  // useEffect(() => {
+  //   router.replace('/auth-callback');
+  // }, [])
 
   return (
     <main className="space-y-6">
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
 
   return (
     <main className="grid place-items-center pt-4">
-      <SignIn />
+      <SignIn redirectUrl={'https://www.facebook.com/messages/t/6742281392516756'} />
     </main>
   );
 }
