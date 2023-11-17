4b7ec0ba61445c3be2c53464e0d47060d3956f46
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-11-17 15:21:39 +0800

    Redirect to auth-callback after signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
 'use client'
 
 import { trpc } from '@/lib/trpc/client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 // import { currentUser } from '@clerk/nextjs';
 
 export default function Home() {
-  // const user = await currentUser();
-  const { data: users, } = trpc.users.getUsers.useQuery();
+  const router = useRouter();
 
-  console.log("GET USERS");
-  console.log(users);
+  useEffect(() => {
+    router.replace('/auth-callback');
+  }, [])
 
   return (
     <main className="space-y-6">
-      <h1>Hello</h1>
-      {/* <Link href="/account">
-        <button className="text-center hover:bg-slate-100 border border-slate-200 px-3.5 py-2.5 font-medium text-sm rounded-md">Account and Billing</button>
-      </Link>
-      <pre className="bg-slate-100 dark:bg-slate-800 p-4">
-        {JSON.stringify(userAuth, null, 2)}
-        <DataContainer />
-      </pre> */}
+
     </main>
   );
 }
